4583fdc8f2698cb1ec620f383ebf90fed3f6cec1
Edmon Jiang <edmon.jiang@gmail.com>
2016-08-31 09:24:12 +0800

    always show vertical scrollbar

--- routes/reports.js
+++ routes/reports.js
@@ -2,17 +2,22 @@ var express = require('express');
 var router = express.Router();
 var Pcinfo = require('../models/pcinfo.js');
 var glob = require('glob');
-var userinfo = {};
-var fs=require('fs');
+var csv = require("fast-csv");
+var fs = require('fs');
 /* GET reports page. */
 
 router.get('/', function(req, res, next){
-  var reports = glob.sync('./public/reports/*',{nodir:true}).map(function(item){return item.split('/').slice(-1)});
+  var reports = glob.sync('./public/reports/*',{nodir:true}).map(
+    function(item){
+      return {filename:item.split('/').slice(-1),
+              created:fs.statSync(item).ctime};
+      }
+    );
   
   res.render('reports', {currentUrl: '/reports', reports: reports});
 })
 
-router.get('/:filename', function(req, res, next){
+router.get('/download/:filename', function(req, res, next){
   var filename = req.params.filename;
   var reports = glob.sync('./public/reports/*',{nodir:true});
   if(filename in reports){
