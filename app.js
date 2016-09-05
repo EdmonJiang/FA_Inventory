4bd8ba6524a6504da168a258969bd0fcf04fd2cc
Edmon Jiang <edmon.jiang@gmail.com>
2016-09-05 09:38:20 +0800

    add compression middleware

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var compression = require('compression');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -23,6 +24,7 @@ var rootFolder = __dirname;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(compression());
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
