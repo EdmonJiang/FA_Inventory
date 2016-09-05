5c5a28f0155ebe4ff6a2eb8eaa3dae280d680ee5
Edmon Jiang <edmon.jiang@gmail.com>
2016-09-05 14:27:35 +0800

    enlarge scale

--- public/javascripts/icharts.js
+++ public/javascripts/icharts.js
@@ -113,7 +113,7 @@ $(function(){
 									position:'bottom',	
 									start_scale:0,
 									end_scale:100,
-									scale_space:20,
+									scale_space:30,
 									listeners:{
 									parseText:function(t,x,y){
 										return {text:t+"%"}
@@ -146,7 +146,7 @@ $(function(){
 						position:'left',//配置左值轴	
 						start_scale:0,//设置开始刻度为0
 						end_scale:26,//设置结束刻度为26
-						scale_space:5,//设置刻度间距
+						scale_space:10,//设置刻度间距
 						listeners:{//配置事件
 						parseText:function(t,x,y){//设置解析值轴文本
 							return {text:t+" pcs"}
