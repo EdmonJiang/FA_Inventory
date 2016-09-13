dfeab42f503d262d197cc2a971f25c0ea95dd0f8
Edmon Jiang <edmon.jiang@gmail.com>
2016-09-13 12:58:46 +0800

    today logs set Hours 0000

--- routes/logs.js
+++ routes/logs.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next){
 
   var weekago = new Date(Date.now() - 7*24*60*60*1000);
   var weekagoDay = new Date(weekago.getFullYear()+"-"+(weekago.getMonth()+1)+"-"+weekago.getDate());
-console.log(weekagoDay)
+
   Pclog.aggregate([{$match:{created:{$gt:weekagoDay}}},{$group:{_id:{month:{$month:"$created"},day:{$dayOfMonth:"$created"}, action:"$action"},total:{$sum:1}}}])
        .exec(function(err, docs){
          if(err){console.log(err)}
@@ -18,11 +18,11 @@ console.log(weekagoDay)
 })
 
 router.post('/', function(req, res, next){
-  var weekago = new Date(Date.now());
-  var weekagoDay = new Date(weekago.getFullYear()+"-"+(weekago.getMonth()+1)+"-"+weekago.getDate());
+  var today = new Date();
+  today.setHours(0,0,0,0);
     // .where('created').gt(moment(Date.now()).format('YYYY-MM-DD'))
   Pclog.find({})
-    .where('created').gt(weekagoDay)
+    .where('created').gt(today)
     .exec(function(err, docs){
       if(err){res.send('error');
               return;}
