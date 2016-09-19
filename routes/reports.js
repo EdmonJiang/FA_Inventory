aad3b33314896cacedc56a0844335240a93618b1
Edmon Jiang <edmon.jiang@gmail.com>
2016-09-19 09:09:06 +0800

    fix reports title

--- routes/reports.js
+++ routes/reports.js
@@ -35,7 +35,7 @@ router.get('/', function(req, res, next){
       reports: reports,
       companies: docs,
       items: Object.keys(Pcinfo.schema.paths),
-      title: "Charts"
+      title: "Reports"
     });
   })
 
