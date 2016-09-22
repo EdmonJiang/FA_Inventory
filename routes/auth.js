95633ade27a55040609ff5206720e2c0d8d86076
Edmon Jiang <edmon.jiang@gmail.com>
2016-09-22 17:32:11 +0800

    add login first flash, close debug

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ module.exports.requireLogin = function(req, res, next){
   if(req.session && req.session.user){
     next();
   }else{
+    req.flash("info", "Please login first.");
     res.redirect('/users/login')
   }
 };
